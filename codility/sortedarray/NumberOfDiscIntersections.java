4ef9aa9df642a6c4e4de30be3ad153a8bcfeba1a
Nagarjun Nagesh <nagarjun_nagesh@outlook.com>
2021-04-01 16:41:46 +0200

    Number of disc interactions test

--- codility/sortedarray/NumberOfDiscIntersections.java
+++ codility/sortedarray/NumberOfDiscIntersections.java
@@ -1,8 +1,5 @@
 package codility.sortedarray;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /* We draw N discs on a plane. The discs are numbered from 0 to N − 1. An array A of N non-negative integers, specifying the radiuses of the discs, is given. The J-th disc is drawn with its center at (J, 0) and radius A[J].
 
 We say that the J-th disc and K-th disc intersect if J ≠ K and the J-th and K-th discs have at least one common point (assuming that the discs contain their borders).
@@ -33,80 +30,93 @@ Write an efficient algorithm for the following assumptions:
 
 N is an integer within the range [0..100,000];
 each element of array A is an integer within the range [0..2,147,483,647].
-
-
-EXAMPLE: 
-
-IF 
-    A[5] = 0
-THEN CREATE ARRAY
-    [5]
-
-IF 
-    A[0] = 1
-THEN CREATE ARRAY
-    [0]
-
-IF
-    A[1] = 5
-THEN CREATE ARRAY
-    [-4,-3,-2,-1,0,1,2,3,4]
-
-ALGORITHM
-    A[P] = K
-IF( K != 0)
-    THEN CREATE AN ARRAY WITH 
-    LOWER LIMIT L = (P - (K-1))
-    UPPER LIMIT U = (P + (K - 1))
-    [L,....,0,...., U]
-ELSE
-    [K]
-
-ARRAYS TO CREATE is N ARRAYS FOR EACH VALUE OF A
-
-And then check if any other array created contains values from other arrays.
-*/
-public class NumberOfDiscIntersections {
-    public static void main(String[] args) {
-        int[] array = { 1, 5, 2, 1, 4, 0 };
+int[] array = { 1, 5, 2, 1, 4, 0 };
         int answer = solution(array);
         System.out.printf("The Expected answer is 11, And the current answer is %s %n", answer);
 
         int[] array1 = { 1, 1, 1 };
         answer = solution(array1);
         System.out.printf("The Expected answer is 3, And the current answer is %s %n", answer);
+
+        int[] array2 = { 1, 2147483647, 0 };
+        answer = solution(array2);
+        System.out.printf("The Expected answer is 2, And the current answer is %s %n", answer);
+
+*/
+public class NumberOfDiscIntersections {
+    public static void main(String[] args) {
+
+        int[] array3 = { 1, 10, 100, 1 };
+        int answer = solution(array3);
+        System.out.printf("The Expected answer is 5, And the current answer is %s %n", answer);
+
     }
 
     public static int solution(int[] circles) {
-        int totalIntersections = 0;
+        // Total Interactions
+        int ti = 0;
         int circleLength = circles.length;
-        Set<Integer> circleIntersectionReference = new HashSet<>();
 
+        if (circles.length == 0) {
+            return ti;
+        }
+
+        // Left Most corner is 0 (Other Values are irrevalent) (As N is a non Negative
+        // Integer)
+        // Calculates number of intersections at each location
+        int[] csp = new int[circleLength];
+        // Right most Corner is (a.length) (Other Values are Irrelevant) (As N is a non
+        // Negative Integer)
+        // Calculates number of intersections at each location
+        int[] cep = new int[circleLength];
+        // Final Position of Array (Ending point array position)
+        int epp = (circleLength - 1);
         for (int i = 0; i < circleLength; i++) {
             int element = circles[i];
+            // Starting Point (Element < 0, then 0, Else SP (i - element))
+            int sp = element < 0 ? 0 : i - element;
+            // SP < 0 choose 0 else choose sp
+            sp = sp < 0 ? 0 : sp;
+            // Ending Point (element > circleLength) ? circleLength else choose (i +
+            // element)
+            int ep = element > epp ? epp : (i + element);
+            // Ending point > circle length choose circle length else ep
+            ep = ep > epp ? epp : ep;
 
-            if (element == 0 || element == 1) {
-                totalIntersections = checkIfIntersected(totalIntersections, circleIntersectionReference, i);
-            } else {
-                int elementMinusOne = (element - 1);
-                int intersectionFarthestToTheLeft = i - elementMinusOne;
-                int intersectionFarthestToTheRight = i + elementMinusOne;
+            // Increase the relevant intersecions
+            csp[sp]++;
+            cep[ep]++;
+        }
 
-                for (int j = intersectionFarthestToTheLeft; j <= intersectionFarthestToTheRight; j++) {
-                    totalIntersections = checkIfIntersected(totalIntersections, circleIntersectionReference, j);
-                }
+        // Active Circle Counter
+        int acc = 0;
+        // Calculate the number of intersections
+        for (int i = 0; i < circleLength; i++) {
+            // Number of Starting Point intersections
+            int spi = csp[i];
+            // Number of Ending point intersections
+            int epi = cep[i];
+
+            if (ti > 10000000) {
+                return -1;
             }
+
+            // Calculate Starting Point together as a single interaction (IF SP = 0 ensure
+            // it adds 0 and not -1)
+            if (spi > 0) {
+                // If 5 circles then (n-1) * (n/2) interactions for each circle (1+2+3+4)
+                // interactions
+                ti += (int) ((spi - 1) * ((float) spi / 2));
+                // Also Register Any Active Circle as interaction
+                ti += spi * acc;
+            }
+            // Calculate Active Circle Counter (Add Strating Point to Active Circle Minus
+            // Ending Point Circle)
+            acc += (spi - epi);
+
         }
 
-        return (totalIntersections > 10000000) ? -1 : totalIntersections;
+        return ti;
     }
 
-    private static int checkIfIntersected(int totalIntersections, Set<Integer> circleIntersectionReference, int i) {
-        if (circleIntersectionReference.contains(i)) {
-            totalIntersections++;
-        } else {
-            circleIntersectionReference.add(i);
-        }
-        return totalIntersections;
-    }
 }
